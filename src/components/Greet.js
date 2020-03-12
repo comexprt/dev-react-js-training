265d375a2d02fad339b1457182e07d7aa0276b09
comxprt <johnnoah.ompad@yahoo.com>
2020-03-13 02:53:42 +0800

    ReactJS Tutorial - 5 - Functional Components

---
+++ src/components/Greet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// basic
+// function Greet(){
+//     return <h1>Hello Vishwas</h1>
+// }
+
+// advance export 
+// export const Greet = () => <h1>Hello Vishwas</h1>
+
+// default export
+const Greet = () => <h1>Hello Vishwas</h1>
+export default Greet
