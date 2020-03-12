a9ea681b8a8bab90d08f6300a2d366c6fd5f6f13
comxprt <johnnoah.ompad@yahoo.com>
2020-03-13 06:09:52 +0800

    ReactJS Tutorial - 18 - Lists and Keys

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
-import Hello from './components/hello'
-import Message from './components/Message'
-import Counter from './components/Counter'
-import FunctionClick from './components/FunctionClick'
-import ClassClick from './components/ClassClick'
-import EventBind from './components/EventBind'
-import ParentComponent from './components/ParentComponent'
-import UserGreeting from './components/UserGreeting'
+// import Greet from './components/Greet'
+// import Welcome from './components/Welcome'
+// import Hello from './components/hello'
+// import Message from './components/Message'
+// import Counter from './components/Counter'
+// import FunctionClick from './components/FunctionClick'
+// import ClassClick from './components/ClassClick'
+// import EventBind from './components/EventBind'
+// import ParentComponent from './components/ParentComponent'
+// import UserGreeting from './components/UserGreeting'
 import NameList from './components/NameList'
 
 
--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Person from './Person'
 
 function NameList(){
-    const names = ['Bruce','Clark','Diana']
+    // const names = ['Bruce','Clark','Diana']
     const persons = [
         {
             id:1,
@@ -11,26 +11,26 @@ function NameList(){
             skill:'React'
         },
         {
-            id:1,
+            id:2,
             name:'Noah',
             age:25,
             skill:'Angular'
         },
         {
-            id:1,
+            id:3,
             name:'Ompad',
             age:28,
             skill:'Vue'
         }
     ]
-    const personList = persons.map(person =>(<Person person={person}></Person>))
+    const personList = persons.map(person =>(<Person key={person.id} person={person}></Person>))
     return(
         <div>
           { personList }
 
         </div>
     )
-    
+
 }
 
 export default NameList 
