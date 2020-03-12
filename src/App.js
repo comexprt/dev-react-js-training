b6a1345a9ad94e5557f801d126ac90f86f564f9f
comxprt <johnnoah.ompad@yahoo.com>
2020-03-13 05:59:24 +0800

    2N8U MINGW64 ~/Desktop/courses/react/hello-world (master)

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import ClassClick from './components/ClassClick'
 import EventBind from './components/EventBind'
 import ParentComponent from './components/ParentComponent'
 import UserGreeting from './components/UserGreeting'
+import NameList from './components/NameList'
+
 
 
 // for advance export
@@ -19,7 +21,7 @@ import UserGreeting from './components/UserGreeting'
 function App() {
   return (
     <div className="App">
-      <UserGreeting/>
+      <NameList/>
  
       {/* <Counter/> */}
       {/* <Message /> */}
---
+++ src/components/NameList.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Person from './Person'
+
+
+function NameList(){
+    const names = ['Bruce','Clark','Diana']
+    const persons = [
+        {
+            id:1,
+            name:'John',
+            age:30,
+            skill:'React'
+        },
+        {
+            id:1,
+            name:'Noah',
+            age:25,
+            skill:'Angular'
+        },
+        {
+            id:1,
+            name:'Ompad',
+            age:28,
+            skill:'Vue'
+        }
+    ]
+
+    const personList = persons.map(person =>(<Person person={person}></Person>))
+    return(
+        <div>
+          { personList }
+
+        </div>
+    )
+}
+
+export default NameList 
