ba27743585ac10cfb3988d25637637d966581a78
comxprt <johnnoah.ompad@yahoo.com>
2020-03-13 05:23:16 +0800

    ReactJS Tutorial - 15 - Methods as props

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import Counter from './components/Counter'
 import FunctionClick from './components/FunctionClick'
 import ClassClick from './components/ClassClick'
 import EventBind from './components/EventBind'
+import ParentComponent from './components/ParentComponent'
+
+
+
 
 
 
@@ -18,7 +22,7 @@ import EventBind from './components/EventBind'
 function App() {
   return (
     <div className="App">
-      <EventBind/>
+      <ParentComponent/>
  
       {/* <Counter/> */}
       {/* <Message /> */}
