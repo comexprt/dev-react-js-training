d5b40fa07d94d8ce59a3407579194b2f79fe1622
comxprt <johnnoah.ompad@yahoo.com>
2020-03-13 06:47:39 +0800

    ReactJS Tutorial - 19 - Index as Key Anti-pattern

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,31 +2,31 @@ import React from 'react'
 import Person from './Person'
 
 function NameList(){
-    // const names = ['Bruce','Clark','Diana']
-    const persons = [
-        {
-            id:1,
-            name:'John',
-            age:30,
-            skill:'React'
-        },
-        {
-            id:2,
-            name:'Noah',
-            age:25,
-            skill:'Angular'
-        },
-        {
-            id:3,
-            name:'Ompad',
-            age:28,
-            skill:'Vue'
-        }
-    ]
-    const personList = persons.map(person =>(<Person key={person.id} person={person}></Person>))
+    const names = ['Bruce','Clark','Diana']
+    // const persons = [
+    //     {
+    //         id:1,
+    //         name:'John',
+    //         age:30,
+    //         skill:'React'
+    //     },
+    //     {
+    //         id:2,
+    //         name:'Noah',
+    //         age:25,
+    //         skill:'Angular'
+    //     },
+    //     {
+    //         id:3,
+    //         name:'Ompad',
+    //         age:28,
+    //         skill:'Vue'
+    //     }
+    // ]
+    const nameList = names.map((name,index) =><h2> key={index}>{index} {name} </h2>)
     return(
         <div>
-          { personList }
+          { nameList }
 
         </div>
     )
