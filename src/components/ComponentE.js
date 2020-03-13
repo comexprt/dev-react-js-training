e9a1fe4a9e676796adc0b78f1d52b4abc63eb918
comxprt <johnnoah.ompad@yahoo.com>
2020-03-14 07:29:38 +0800

    ReactJS Tutorial - 40 - Context (Part 3)

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 import ComponentF from './ComponentF'
+import UserContext from './userContext'
 
 class ComponentE extends Component{
+    static contextType =  UserContext
 
   render(){
-    return <ComponentF />
+    return (
+        <div>
+            Component E context {this.context}
+            <ComponentF />
+        </div>
+    )
   }
 }
 
--- src/components/userContext.js
+++ src/components/userContext.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const UserContext = React.createContext()
+const UserContext = React.createContext('Web Dev')
 
 const UserProvider = UserContext.Provider
 const UserConsumer = UserContext.Consumer
 
 export {UserProvider, UserConsumer}
+
+export default UserContext
