73a0f423004fac68e6cc47c2aca6d3ea107ad81a
comxprt <johnnoah.ompad@yahoo.com>
2020-03-14 05:07:18 +0800

    ReactJS Tutorial - 36 - Render Props (Part 1)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import './App.css';
-
-import ClickCounter from './components/ClickCounter';
-import HoverCounter from './components/HoverCounter';
+import ClickCounterTwo from './components/ClickCounterTwo';
+import HoverCounterTwo from './components/HoverCounterTwo';
 
 
 class App extends Component{
@@ -10,8 +9,8 @@ class App extends Component{
   render(){
     return (
       <div className="App">
-        <ClickCounter name="John"/>
-        <HoverCounter/>
+        <ClickCounterTwo/>
+        <HoverCounterTwo />
       </div>
       )
   }
