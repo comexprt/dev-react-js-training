89c58fdfb92fb9074d78f195de500370ae0240f2
comxprt <johnnoah.ompad@yahoo.com>
2020-03-14 03:29:44 +0800

    ReactJS Tutorial - 30 - Forwarding Refs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import ParentComp from './components/ParentComp';
 import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
+import FRParentInput from './components/FRParentInput';
 // import Input from './components/Input';
 
 
@@ -12,7 +13,7 @@ class App extends Component{
   render(){
     return (
       <div className="App">
-         <FocusInput/>
+         <FRParentInput  />
       </div>
       )
   }
