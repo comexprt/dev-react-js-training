4398cc25997f10b747a44966d8880eab55475cab
comxprt <johnnoah.ompad@yahoo.com>
2020-03-14 08:02:04 +0800

    ReactJS Tutorial - 42 - HTTP GET Request - updated

--- src/newComponents/PostList.js
+++ src/newComponents/PostList.js
@@ -13,7 +13,7 @@ class PostList extends Component{
     }
 
     componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts1')
+        axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
                 console.log(response)
                 this.setState({posts:response.data})
