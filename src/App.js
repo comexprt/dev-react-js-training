279ecf8f2f45898fa2d540cc6392df7f586949e7
comxprt <johnnoah.ompad@yahoo.com>
2020-03-17 02:07:50 +0800

    React Hooks Tutorial - 2 - useState Hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './App.css';
-import PostForm from './newComponents/PostForm';
-// import PostList from './newComponents/PostList';
+import ClassCounter from './hookComponents/ClassCounter';
+import HookCounter from './hookComponents/HookCounter';
 
 
 
@@ -10,9 +10,8 @@ class App extends Component{
   render(){
     return (
       <div className="App">
-        {/* <PostList/> */}
-        <PostForm />
-          
+          {/* <ClassCounter /> */}
+          <HookCounter />
       </div>
       )
   }
