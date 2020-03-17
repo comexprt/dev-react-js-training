56ca76b4d9add275bb2f90b504d0d28c5a1536d9
comxprt <johnnoah.ompad@yahoo.com>
2020-03-18 01:21:29 +0800

    React Hooks Tutorial - 29 - useRef Hook Part 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import './App.css';
 import FocusInput from './hookComponents/FocusInput';
+import ClassTimer from './hookComponents/ClassTimer';
+import HookTimer from './hookComponents/HookTimer';
 
 
 
 function App() {
   return (
     <div className='App'>
-       <FocusInput />
+       {/* <FocusInput /> */}
+       <ClassTimer />
+       <HookTimer />
     </div>
   )
 }
